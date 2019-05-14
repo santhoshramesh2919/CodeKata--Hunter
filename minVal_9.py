07724aa7b6856bb779861d36dae1bf3723c24c9b
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-14 10:28:23 +0530

    Create minVal_9.py

---
+++ minVal_9.py
@@ -0,0 +1,16 @@
+def func(li):
+  min=[]
+  for i in range(0,len(li)-1):
+    for j in range(i+1,len(li)):
+        if li[i]+li[j]==0:
+          print(li[i],li[j])
+          return
+        elif min==[]:
+          min=[li[i],li[j]]
+        else:
+          if li[i]+li[j]<sum(min):
+            min=[li[i],li[j]]
+  return min
+n=int(input())
+li=list(map(int,input().split()))
+op=func(li)
