d34abf6709c3c4db645bcf067d3ba893902f6f50
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-14 09:45:07 +0530

    Update evnInOddPos_7.py

--- evnInOddPos_7.py
+++ evnInOddPos_7.py
@@ -6,5 +6,8 @@ for i in range(len(arr)):
     op.append(arr[i])
   elif arr[i]%2==1 and i%2==0:
     op.append(arr[i])
-
-print(*op)
+for i in range(0,len(op)):
+  if(i==len(op)-1):
+    print(op[i])
+  else:
+    print(op[i],end=" ")
