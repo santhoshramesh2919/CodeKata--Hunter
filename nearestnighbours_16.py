c33c56af280e4b40cee7db870a534b4b36825808
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-15 10:16:14 +0530

    Create nearestnighbours_16.py

---
+++ nearestnighbours_16.py
@@ -0,0 +1,15 @@
+n,k=map(int,input().split(" "))
+li=list(map(int,input().split()))
+dic={}
+op=[]
+li.pop(li.index(k))
+for i in li:
+  dic[str(i)]=abs(i-k)
+d = sorted((value, key) for (key,value) in dic.items())
+for i in d:
+  op.append(i[1]))
+for i in range(0,2):
+  if i==2:
+    print(op[2])
+  else:
+    print(op[i],end=" ")
