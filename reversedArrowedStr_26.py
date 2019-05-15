ffc0a2ec2cfee9bc695c948f098c335436cc7cdd
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-15 15:33:41 +0530

    Update reversedArrowedStr_26.py

--- reversedArrowedStr_26.py
+++ reversedArrowedStr_26.py
@@ -1,9 +1,5 @@
 n=int(input())
 li=list(map(int,input().split()))
-s=""
-for i in range(len(li)-1,-1,-1):
-  if i==0:
-    s=s+str(li[i])
-  else:
-    s=s+str(li[i])+"->"
-print(s)
+for i in range(len(li)-1,0,-1):
+  print(li[i],end="->")
+print(li[i-1])
