9db149b6b091c501796288791c7b88ffd90b6dc8
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-15 13:52:28 +0530

    Update CommInAllArr_19.py

--- CommInAllArr_19.py
+++ CommInAllArr_19.py
@@ -3,15 +3,19 @@ li=[]
 op=[]
 for i in range(n):
   li.append(list(map(int,input().split())))
-for i in range(k):
-  if li[0][i] in li[1]:
-    op.append(li[0][i])
+if n==1:
+  for i in li:
+    print(li[0][0])
+else:
+  for i in range(k):
+    if li[0][i] in li[1]:
+      op.append(li[0][i])
 
-for i in range(2,n):
-  res=[]
-  for j in range(len(op)):
-    if op[j] in  li[i]:
-      res.append(op[j])
-  op=res
-op=set(op)
-print(*op)
+  for i in range(2,n):
+    res=[]
+    for j in range(len(op)):
+      if op[j] in  li[i]:
+        res.append(op[j])
+    op=res
+  op=set(op)
+  print(*op)
