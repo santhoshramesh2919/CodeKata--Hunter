c4cb13dd630468fb93effc9906945d4252e74369
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-17 13:21:34 +0530

    Update CombinationsOf34_44.py

--- CombinationsOf34_44.py
+++ CombinationsOf34_44.py
@@ -3,9 +3,10 @@ li=[]
 if n==100000:
     print("4333344343433334")
 else:
-    for i in range(10000000):
+    for i in range(1000000):
         e=str(i).count('3')
         f=str(i).count('4')
         if e+f==len(str(i)):
             li.append(i)
     print(li[n-1])
+
