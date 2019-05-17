05a146a37588df8fe3fd24f4ad2bc530e5759fe2
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-17 09:01:46 +0530

    Update ProdOfMaxSubArr_31.py

--- ProdOfMaxSubArr_31.py
+++ ProdOfMaxSubArr_31.py
@@ -1,10 +1,15 @@
-import  numpy
+def prod(lis):
+  res=1
+  for i in lis:
+    res=res*i
+  return res
+
 n=int(input())
 li=list(map(int,input().split()))
 m=min(li)
 for i in range(len(li)-1):
   for j in range(i,len(li)):
-    s=(numpy.prod(li[i:j+1]))
+    s=(prod(li[i:j+1]))
     if s>m:
       m=s
 print(m)
