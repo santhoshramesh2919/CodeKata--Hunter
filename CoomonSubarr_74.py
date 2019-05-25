3e429e3944e3dbf870fd345b2e819e17b4b16041
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-25 22:45:54 +0530

    Update CoomonSubarr_74.py

--- CoomonSubarr_74.py
+++ CoomonSubarr_74.py
@@ -1,10 +1,12 @@
-#SAmw 
 s1=input()
 s2=input()
-op=[]
+t=""
+r=[]
+n=0
 for i in range(len(s1)-1):
-  for j in range(i+1,len(s1)):
-    subarr=s1[i:j+1]
-    if subarr in s2:
-      op.append(subarr)
-print(max(op,key=len))
+	for j in range(i,len(s1)):
+		if s1[i:j+1] in s2:
+			if len(s1[i:j+1])>=len(t):
+				t=s1[i:j+1]
+				a,b=i,j
+print(s1[a:b+1])
